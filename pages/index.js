6ec3fbc073fe1092ffdfbdfabcb45e04ac4c848c
nikolay-waveo <nikolay@waveo.com>
2021-12-30 11:35:54 +0800

    Feat: Move existing files into project

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import { Heading, Page } from "@shopify/polaris";
-
-const Index = () => (
-  <Page>
-    <Heading>Perkd App 0.1 🎉</Heading>
-  </Page>
-);
-
-export default Index;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index: React.FC = () => {
+  return (
+    <Page>
+      <Heading>Perkd App 0.2 🎉</Heading>
+    </Page>
+  )
+}
+  
+export default Index;
